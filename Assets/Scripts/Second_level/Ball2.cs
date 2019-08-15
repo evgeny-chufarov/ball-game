594e0c1c636edf968490291eef9b67be6116084f
Evgeny Chufarov <evgeny.chufarov.1@gmail.com>
2019-08-15 23:50:20 +0200

    123

--- Assets/Scripts/Second_level/Ball2.cs
+++ Assets/Scripts/Second_level/Ball2.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class Ball2 : MonoBehaviour
-    {
-        private Rigidbody2D rb;
-           
-        public float baseSpeed = 1.5f;
-        public static float speed;
-        public bool am_i_faster = false;
+public class Ball2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+        
+    // Variables for speed control
+    public float baseSpeed = 1.5f;
+    public static float speed;
+    public bool am_i_faster = false;
     
-        // Use this for initialization
     void Start()
-        {
+    {
             rb = GetComponent<Rigidbody2D>();
             speed = baseSpeed;
-        }
+    }
 
-    // Update is called once per frame
     void Update()
-        {
+    {
             // Get player's key
             float moveHorizontal = Input.GetAxis("Horizontal");
 
@@ -36,14 +35,15 @@ using UnityEngine;
                 rb.AddForce(jump, ForceMode2D.Impulse);
             }
 
+            // Visualising speed altering
             if (speed == 4)
             {
                 am_i_faster = true;
             }
             else
             {
-            am_i_faster = false;
+                am_i_faster = false;
             }
 
-        }
+    }
 }
